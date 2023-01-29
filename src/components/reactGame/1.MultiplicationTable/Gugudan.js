70e2e177a040b2faf432235ecb029440f68f767a
kim-hyeona <kimha9@naver.com>
2023-01-29 22:00:08 +0900

    props.children

---
+++ src/components/reactGame/1.MultiplicationTable/Gugudan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Gugudan(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Gugudan;
--- src/components/udemyReact/ExpenseItem.js
+++ src/components/udemyReact/ExpenseItem.js
@@ -1,4 +1,5 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card"
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
@@ -7,18 +8,13 @@ function ExpenseItem(props) {
     // const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item">
-                 {/* <div>
-                    <div>{month}</div>
-                    <div>{day}</div>
-                    <div>{year}</div>
-                </div> */}
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
          <div className="expense-item__description" >
             <h2>{props.title}</h2>
             <div className="expense-item__price" >{props.amount}</div>
          </div>
-        </div>
+        </Card>
     );
 }
 

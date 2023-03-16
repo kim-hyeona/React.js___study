07cc23d8de7d407370f861fed5deb13f5324db60
kim-hyeona <kimha9@naver.com>
2023-03-16 14:56:18 +0900

    map

--- src/components/udemyReact/NewEX/NewExpense.js
+++ src/components/udemyReact/NewEX/NewExpense.js
@@ -1,14 +1,22 @@
 import React from 'react';
+
 import ExpenseForm from './ExpenseForm';
-import "./NewExpense.css"
+import './NewExpense.css';
 
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
 
-function NewExpense(props) {
-    return (
-        <div className='new-expense'>
-           <ExpenseForm/>
-        </div>
-    );
-}
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
 
 export default NewExpense;
